6ddd9eabb6fbdc9e17765ea38f1947b94f3c6865
ahmedosman1985 <ahmed.osman.va@gmail.com>
2023-04-22 11:26:54 -0400

    Smoke Runner created

---
+++ src/test/java/TestRunner/SmokeRunner.java
@@ -0,0 +1,13 @@
+package TestRunner;
+
+
+//@RunWith(Cucumber.class)
+//@CucumberOptions(features = "src/test/resources/Features" ,
+// glue = "StepDefinitions",
+// dryRun = false,
+// tags = "@testcase',
+// plugin = {"pretty,"html:target/Cucumber.html","json:target/Cucumber.json","rerun:target/failed.txt"}
+// )
+public class SmokeRunner {
+
+}
